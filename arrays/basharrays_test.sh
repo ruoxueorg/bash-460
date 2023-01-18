ae9209dfec157bf441bfe62153c3dcf47fe10c26
ruoxue <ruoxue777@gmail.com>
2023-01-18 11:04:36 +0800

    update test

--- arrays/basharrays_test.sh
+++ arrays/basharrays_test.sh
@@ -23,6 +23,38 @@ testGet() {
     assertEquals "get" $expected ${array[1]}
 }
 
+testDelete() {
+    expectedSize=2
+    array=("Apple" "Banana" "Cherry")
+    unset array[0]
+    echo ${array[@]}
+    assertEquals "length" $expectedSize ${#array[@]}
+}
+
+testLength() {
+    expectedSize=3
+    array=("Apple" "Banana" "Cherry")
+    echo ${#array[@]}
+    assertEquals "length" $expectedSize ${#array[@]}
+}
+
+testLoop() {
+    array=("Apple" "Banana" "Cherry")
+    for i in "${array[@]}"
+    do
+        echo $i
+    done
+}
+
+testSet() {
+    expected="Grape"
+    array=("Apple" "Banana" "Cherry")
+    echo ${array[@]}
+    array[0]="Grape"
+    echo ${array[@]}
+    assertEquals "get" $expected ${array[0]}
+}
+
 
 # Load shunit2
 . shunit2
