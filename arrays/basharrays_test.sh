4c3b840d42b01689be7629d5a8ca7e0e5917fb39
ruoxue <ruoxue777@gmail.com>
2023-01-18 10:51:12 +0800

    update test

--- arrays/basharrays_test.sh
+++ arrays/basharrays_test.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-testAdd(){
-	expectedSize = 3
+testAdd() {
+    expectedSize=3
     array=("Apple" "Banana" "Cherry")
-    echo ${array[*]}
-    assertEquals "Equals" ${#array[*]} expectedSize
-    
+    echo ${array[@]}
+    assertEquals "length" $expectedSize ${#array[@]}
 }
+
+testAddByIndex() {
+    expectedSize=4
+    array=("Apple" "Banana" "Cherry")
+    #array+=("Grape")
+    array[3]="Grape"
+    echo ${array[@]}
+    assertEquals "length" $expectedSize ${#array[@]}
+}
+
+testGet() {
+    expected="Banana"
+    array=("Apple" "Banana" "Cherry")
+    echo ${array[@]}
+    assertEquals "get" $expected ${array[1]}
+}
+
 
 # Load shunit2
 . shunit2
