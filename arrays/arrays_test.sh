09e416206e6cade8f6694a92985987d952fbde67
ruoxue <ruoxue777@gmail.com>
2023-01-24 19:44:18 +0800

    update arrays

--- arrays/arrays_test.sh
+++ arrays/arrays_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 testAdd () {
-    expectedLength=3
-    array=("Apple" "Banana" "Cherry")
+    local expectedLength=3
+    local array=("Apple" "Banana" "Cherry")
     echo ${array[@]}
     assertEquals "length" $expectedLength ${#array[@]}
 }
 
 testAddByIndex () {
-    expectedLength=4
-    array=("Apple" "Banana" "Cherry")
+    local expectedLength=4
+    local array=("Apple" "Banana" "Cherry")
     #array+=("Grape")
     array[3]="Grape"
     echo ${array[@]}
@@ -17,37 +17,37 @@ testAddByIndex () {
 }
 
 testGet () {
-    expected="Banana"
-    array=("Apple" "Banana" "Cherry")
+    local expected="Banana"
+    local array=("Apple" "Banana" "Cherry")
     echo ${array[1]}
     assertEquals "get" $expected ${array[1]}
 }
 
 testDelete () {
-    expectedLength=2
-    array=("Apple" "Banana" "Cherry")
+    local expectedLength=2
+    local array=("Apple" "Banana" "Cherry")
     unset array[0]
     echo ${array[@]}
     assertEquals "length" $expectedLength ${#array[@]}
 }
 
 testDeleteAll () {
-    expectedLength=0
-    array=("Apple" "Banana" "Cherry")
+    local expectedLength=0
+    local array=("Apple" "Banana" "Cherry")
     unset array
     echo ${!array[@]}
     assertEquals "length" $expectedLength ${#array[@]}
 }
 
 testLength () {
-    expectedLength=3
-    array=("Apple" "Banana" "Cherry")
+    local expectedLength=3
+    local array=("Apple" "Banana" "Cherry")
     echo ${#array[@]}
     assertEquals "length" $expectedLength ${#array[@]}
 }
 
 testLoop () {
-    array=("Apple" "Banana" "Cherry")
+    local array=("Apple" "Banana" "Cherry")
     for i in "${array[@]}"
     do
         echo $i
@@ -55,8 +55,8 @@ testLoop () {
 }
 
 testSet () {
-    expected="Grape"
-    array=("Apple" "Banana" "Cherry")
+    local expected="Grape"
+    local array=("Apple" "Banana" "Cherry")
     echo ${array[@]}
     array[0]="Grape"
     echo ${array[@]}
@@ -64,9 +64,9 @@ testSet () {
 }
 
 testSlice() {
-    expectedLength=2
-    array=("Apple" "Banana" "Cherry")
-    slice_array=("${array[@]:1:2}")
+    local expectedLength=2
+    local array=("Apple" "Banana" "Cherry")
+    local slice_array=("${array[@]:1:2}")
     echo ${slice_array[@]}
     assertEquals "length" $expectedLength ${#slice_array[@]}
 }
